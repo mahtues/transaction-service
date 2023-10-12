fd8b486cb89142aadfb5962aca6d864d3eb53574
mahtues <14203456+mahtues@users.noreply.github.com>
2023-10-12 11:34:09 -0300

    Configuring test workflow (#1)
    Adding test action in pull requests.

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFoo(t *testing.T) {
+	t.Run("successful test", func(t *testing.T) {
+		expected := 5
+
+		actual := foo(2, 3)
+
+		if actual != expected {
+			t.Errorf("expected: %v, actual: %v", expected, actual)
+		}
+	})
+}
