77a8ee07ebd979c7c90f0dbcbec2611653dc3308
mahtues <14203456+mahtues@users.noreply.github.com>
2023-10-13 19:39:52 -0300

    [Feature] Transaction Service (#5)
    * partial implementation of service

--- transaction/service.go
+++ transaction/service.go
@@ -4,18 +4,11 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/pkg/errors"
+
 	"github.com/mahtues/transaction-service/conversion"
+	"github.com/mahtues/transaction-service/random"
 )
-
-type CreateRequest struct {
-	Description string
-	Date        time.Time
-	AmountUs    string
-}
-
-type CreateResponse struct {
-	Id string
-}
 
 type Service struct {
 	repository Repository
@@ -28,10 +21,65 @@ func NewServiceWithDynamo(client *dynamodb.DynamoDB, tableName string) Service {
 	}
 }
 
-func (s Service) GetTransaction(id string) error {
-	return nil
+func NewService(repository Repository, conversion conversion.Service) *Service {
+	return &Service{
+		repository: repository,
+		conversion: conversion,
+	}
 }
 
-func (s Service) CreateTransaction() error {
-	return nil
+func (s *Service) CreateTransaction(request CreateRequest) (CreateResponse, error) {
+	transaction := Transaction{
+		Id:          "tr-" + random.String(20),
+		Description: request.Description,
+		Date:        request.Date,
+		AmountUs:    request.AmountUs,
+	}
+
+	if err := s.repository.SaveTransaction(transaction); err != nil {
+		return CreateResponse{}, errors.Wrap(err, "failed to save transaction")
+	}
+
+	return CreateResponse{
+		Id: transaction.Id,
+	}, nil
+}
+
+func (s *Service) GetTransaction(request GetRequest) (GetResponse, error) {
+	transaction, err := s.repository.LoadTransaction(request.Id)
+	if err != nil {
+		return GetResponse{}, errors.Wrapf(err, "failed to load transaction id %v", request.Id)
+	}
+
+	return GetResponse{
+		Id:              transaction.Id,
+		Description:     transaction.Description,
+		Date:            transaction.Date,
+		AmountUs:        transaction.AmountUs,
+		Rate:            "",
+		AmountConverted: "",
+	}, nil
+}
+
+type CreateRequest struct {
+	Description string
+	Date        time.Time
+	AmountUs    string
+}
+
+type CreateResponse struct {
+	Id string
+}
+
+type GetRequest struct {
+	Id string
+}
+
+type GetResponse struct {
+	Id              string
+	Description     string
+	Date            time.Time
+	AmountUs        string
+	Rate            string
+	AmountConverted string
 }
--- transaction/service_test.go
+++ transaction/service_test.go
@@ -2,17 +2,75 @@ package transaction
 
 import (
 	"testing"
+
+	"github.com/mahtues/transaction-service/conversion"
+	"github.com/pkg/errors"
 )
 
 func TestUnit(t *testing.T) {
-	s := NewServiceWithDynamo(nil, "")
+	base := NewService(nil, conversion.Service{})
 
 	t.Run("success", func(t *testing.T) {
-		var expected, actual error = nil, s.CreateTransaction()
+		s := newServiceWithMockRepository(base, &mockRepository{
+			saveTransactionFunc: func(transaction Transaction) error {
+				if transaction.Id == "" {
+					return errors.New("transaction id not initialized")
+				}
 
-		if actual != expected {
-			t.Errorf("expected: %v, actual: %v", expected, actual)
+				return nil
+			},
+		})
+
+		actual, actualErr := s.CreateTransaction(CreateRequest{})
+
+		if actualErr != nil {
+			t.Errorf("expected: %v, actual: %v", nil, actualErr)
+		}
+
+		if actual.Id == "" {
+			t.Errorf("expected: %v, actual: %v", "random id", actual)
 		}
 	})
 
+	t.Run("repository save error", func(t *testing.T) {
+		s := newServiceWithMockRepository(base, &mockRepository{
+			saveTransactionFunc: func(t Transaction) error {
+				return errors.New("save error")
+			},
+		})
+
+		_, actualErr := s.CreateTransaction(CreateRequest{})
+
+		if actualErr == nil {
+			t.Errorf("expected: %v, actual: %v", "not nil", actualErr)
+		}
+	})
+}
+
+type mockRepository struct {
+	saveTransactionFunc func(Transaction) error
+	loadTransactionFunc func(id string) (Transaction, error)
+	next                Repository
+}
+
+func newServiceWithMockRepository(service *Service, mock *mockRepository) *Service {
+	newService := *service
+	mock.next = service.repository
+	newService.repository = mock
+
+	return &newService
+}
+
+func (m *mockRepository) SaveTransaction(transaction Transaction) error {
+	if m.saveTransactionFunc == nil {
+		return m.next.SaveTransaction(transaction)
+	}
+	return m.saveTransactionFunc(transaction)
+}
+
+func (m *mockRepository) LoadTransaction(id string) (Transaction, error) {
+	if m.loadTransactionFunc == nil {
+		return m.next.LoadTransaction(id)
+	}
+	return m.loadTransactionFunc(id)
 }
