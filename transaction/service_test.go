610d209c5e897b9336f921423f5976adbfba10f9
mahtues <14203456+mahtues@users.noreply.github.com>
2023-10-13 18:33:42 -0300

    tests

--- transaction/service_test.go
+++ transaction/service_test.go
@@ -2,17 +2,63 @@ package transaction
 
 import (
 	"testing"
+
+	"github.com/pkg/errors"
 )
 
+type mockRepository struct {
+	saveTransactionFunc func(Transaction) error
+	loadTransactionFunc func(id string) (Transaction, error)
+}
+
+func (m *mockRepository) SaveTransaction(transaction Transaction) error {
+	return m.saveTransactionFunc(transaction)
+}
+
+func (m *mockRepository) LoadTransaction(id string) (Transaction, error) {
+	return m.loadTransactionFunc(id)
+}
+
+func newServiceWithMockRepository(mock *mockRepository) Service {
+	return Service{
+		repository: mock,
+	}
+}
+
 func TestUnit(t *testing.T) {
-	s := NewServiceWithDynamo(nil, "")
-
 	t.Run("success", func(t *testing.T) {
-		var expected, actual error = nil, s.CreateTransaction()
+		s := newServiceWithMockRepository(&mockRepository{
+			saveTransactionFunc: func(transaction Transaction) error {
+				if transaction.Id == "" {
+					return errors.New("transaction id not initialized")
+				}
 
-		if actual != expected {
-			t.Errorf("expected: %v, actual: %v", expected, actual)
+				return nil
+			},
+		})
+
+		actual, actualErr := s.CreateTransaction(CreateRequest{})
+
+		if actualErr != nil {
+			t.Errorf("expected: %v, actual: %v", nil, actualErr)
+		}
+
+		if actual.Id == "" {
+			t.Errorf("expected: %v, actual: %v", "random id", actual)
 		}
 	})
 
+	t.Run("repository save error", func(t *testing.T) {
+		s := newServiceWithMockRepository(&mockRepository{
+			saveTransactionFunc: func(t Transaction) error {
+				return errors.New("save error")
+			},
+		})
+
+		_, actualErr := s.CreateTransaction(CreateRequest{})
+
+		if actualErr == nil {
+			t.Errorf("expected: %v, actual: %v", "not nil", actualErr)
+		}
+	})
 }
