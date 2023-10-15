19eaceebaabdb8227577c2a5b7ed60a1b7337cf1
mahtues <14203456+mahtues@users.noreply.github.com>
2023-10-15 11:30:02 -0300

    asd

--- transaction/repo.go
+++ transaction/repo.go
@@ -15,3 +15,11 @@ type Repository interface {
 	SaveTransaction(transaction Transaction) error
 	LoadTransaction(id string) (Transaction, error)
 }
+
+func (this Transaction) Equal(that Transaction) bool {
+	e := this.Id == that.Id
+	e = e && this.Description == that.Description
+	e = e && this.Date.Equal(that.Date)
+	e = e && this.AmountUs == that.AmountUs
+	return e
+}
